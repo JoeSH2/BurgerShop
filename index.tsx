c30aa60c6b5465aca2a5b9fc9c8e79b72ef6897e
JoeSH2 <106405021+JoeSH2@users.noreply.github.com>
2022-08-31 16:40:05 +0300

    Add files via upload

---
+++ index.tsx
@@ -0,0 +1,71 @@
+import React, { FC, memo, useMemo } from "react";
+import { useDispatch } from "react-redux";
+import { addCartProduct } from "../../redux/cartSlice";
+import { productType } from "../../redux/fetchSlice/fetchSliceType";
+import { addProductModal } from "../../redux/modalSlice";
+import style from "./productCard.module.scss";
+
+export interface IProduct {
+  item: productType;
+  openModal?: any;
+}
+
+const BurgerCard: FC<IProduct> = memo(({ item, openModal }) => {
+  const dispatch = useDispatch();
+
+  const productValue = useMemo(() => {
+    return {
+      id: item.id,
+      category: item.category,
+      imageUrl: item.imageUrl,
+      title: item.title,
+      price: item.price,
+      description: item.description,
+      mainDescription: item.mainDescription,
+      titleDoping: item.titleDoping,
+      dopings: item.dopings,
+      dopingsPrice: item.dopingsPrice,
+      titleDoping2: item.titleDoping2,
+      dopings2: item.dopings2,
+      count: 0,
+    };
+  }, [item]);
+
+  const productCartValue = useMemo(() => {
+    return {
+      id: item.id,
+      category: item.category,
+      imageUrl: item.imageUrl,
+      title: item.title,
+      price: item.price,
+      dopings: item.dopings[0],
+      dopings2: item.dopings2[0],
+      count: 0,
+    };
+  }, [item]);
+
+  const clickModale = () => {
+    dispatch(openModal);
+    dispatch(addProductModal(productValue));
+  };
+  const clickAddCart = () => {
+    dispatch(addCartProduct(productCartValue));
+  };
+  return (
+    <div className={style.card}>
+      <div onClick={clickModale} className={style.cardWrapper}>
+        <img className={style.cardImg} src={item.imageUrl} alt="Фото товара" />
+        <div className={style.discription}>
+          <div className={style.cardTitle}>{item.title}</div>
+          <div className={style.cardPrice}>{item.price}Р</div>
+          <div className={style.cardText}>{item.mainDescription}</div>
+        </div>
+      </div>
+      <button onClick={clickAddCart} className={style.btn}>
+        Выбрать
+      </button>
+    </div>
+  );
+});
+
+export default BurgerCard;
